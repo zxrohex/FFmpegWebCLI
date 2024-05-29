adf20cda8b5977f83546e566bfa2db110a70f0da
Lynn <Lynn@DESKTOP-P0HJMGA>
2024-05-29 14:00:15 +0200

    Add project files.

---
+++ FFmpegWebCLI.App/Core/JS/JSFFmpegManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.JSInterop;
+
+namespace FFmpegWebCLI.App.Core.JS
+{
+    public class JSFFmpegManager
+    {
+        private IJSRuntime jsRuntime;
+
+        private IJSObjectReference jsModule;
+
+        private DotNetObjectReference<JSFFmpegManager> instanceObjRef;
+
+
+
+        public JSFFmpegManager(IJSRuntime jsRuntime)
+        {
+            this.jsRuntime = jsRuntime;
+
+            this.instanceObjRef = DotNetObjectReference.Create(this);
+
+            Initialize();
+        }
+
+        private async void Initialize()
+        {
+            jsModule = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./js/app-core.js");
+
+            await jsModule.InvokeVoidAsync("init", instanceObjRef);
+        }
+
+
+        [JSInvokable]
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ FFmpegWebCLI.App/wwwroot/js/app-core.js
@@ -0,0 +1,18 @@
+﻿import { FFmpeg } from "./libs/ffmpeg/main/dist/esm/index.js";
+import { fetchFile, toBlobURL } from "./libs/ffmpeg/util/dist/esm/index.js";
+
+var ffmpeg = new FFmpeg();
+
+export async function init(objRef) {
+
+    ffmpeg.on("log", ({ message }) => {
+        objRef.invokeMethodAsync("LogMessage", message);
+    });
+
+    await ffmpeg.load({
+        coreURL: "/js/libs/ffmpeg/core/dist/esm/ffmpeg-core.js",
+        wasmURL: "/js/libs/ffmpeg/core/dist/esm/ffmpeg-core.wasm"
+    });
+
+    await ffmpeg.exec("-h");
+}
